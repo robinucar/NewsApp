f5498d0ee8448726d8ebef6d0952aafc2ebabef1
robinucar <mehmet.ucar@imediapro.co.uk>
2020-10-28 20:18:00 +0000

    Login component

---
+++ src/Login.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { SafeAreaView, View, Text } from 'react-native'; 
+
+const Login = () => {
+    return (
+        <SafeAreaView>
+            <View>
+                <Text>Login</Text>
+            </View>
+        </SafeAreaView>
+    )
+}
+
+export default Login
