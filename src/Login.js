de5c854cda142becdc497861a611ed075abed420
robinucar <mehmet.ucar@imediapro.co.uk>
2020-10-28 21:57:04 +0000

    icon added and styled

--- src/Login.js
+++ src/Login.js
@@ -1,14 +1,33 @@
 import React from 'react';
-import { SafeAreaView, View, Text } from 'react-native'; 
+import {
+  SafeAreaView,
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  Dimensions,
+} from 'react-native';
 
 const Login = () => {
-    return (
-        <SafeAreaView>
-            <View>
-                <Text>Login</Text>
-            </View>
-        </SafeAreaView>
-    )
-}
+  return (
+    <SafeAreaView>
+      <View>
+        <Image
+          source={require('./assests/login.png')}
+          style={styles.logoStyle}
+        />
+        <Text>Login</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
 
-export default Login
+export default Login;
+
+const styles = StyleSheet.create({
+  logoStyle: {
+    backgroundColor: 'red',
+    resizeMode: 'contain',
+    width: Dimensions.get('window').width,
+  },
+});
